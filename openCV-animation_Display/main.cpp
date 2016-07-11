956c211c784c724e09eb3f2424645c00a5b9cdd2
Alan Vitullo <Hal9000@hal-9000.local>
2016-07-11 12:46:59 -0700

    Initial Commit

---
+++ openCV-animation_Display/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  openCV-animation_Display
+//
+//  Created by Alan Vitullo on 7/11/16.
+//  Copyright © 2016 Alan Vitullo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
